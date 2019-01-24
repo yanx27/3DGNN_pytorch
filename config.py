1a37b116ea48c6291218d279fa699e7e318baa45
benny <35829331+yanx27@users.noreply.github.com>
2019-01-24 23:29:21 +0800

    configuration

--- config.py
+++ config.py
@@ -28,3 +28,275 @@ weight_decay = 1e-4
 lr_schedule_type = 'exp'
 lr_decay = 0.9
 lr_patience = 10
+
+ENCODER_PARAMS = [
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 32,
+        'output_channels': 64,
+        'downsample': True,
+        'dropout_prob': 0.01
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 64,
+        'output_channels': 64,
+        'downsample': False,
+        'dropout_prob': 0.01
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 64,
+        'output_channels': 64,
+        'downsample': False,
+        'dropout_prob': 0.01
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 64,
+        'output_channels': 64,
+        'downsample': False,
+        'dropout_prob': 0.01
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 64,
+        'output_channels': 64,
+        'downsample': False,
+        'dropout_prob': 0.01
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 64,
+        'output_channels': 128,
+        'downsample': True,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 2
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 4
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 8
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 16
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 2
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 4
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 8
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    },
+    {
+        'internal_scale': 4,
+        'use_relu': True,
+        'asymmetric': False,
+        'dilated': False,  # 16
+        'input_channels': 128,
+        'output_channels': 128,
+        'downsample': False,
+        'dropout_prob': 0.1
+    }
+]
+
+DECODER_PARAMS = [
+    {
+        'input_channels': 128,
+        'output_channels': 128,
+        'upsample': False,
+        'pooling_module': None
+    },
+    {
+        'input_channels': 128,
+        'output_channels': 64,
+        'upsample': True,
+        'pooling_module': None
+    },
+    {
+        'input_channels': 64,
+        'output_channels': 64,
+        'upsample': False,
+        'pooling_module': None
+    },
+    {
+        'input_channels': 64,
+        'output_channels': 64,
+        'upsample': False,
+        'pooling_module': None
+    },
+    {
+        'input_channels': 64,
+        'output_channels': 32,
+        'upsample': True,
+        'pooling_module': None
+    },
+    {
+        'input_channels': 32,
+        'output_channels': 32,
+        'upsample': False,
+        'pooling_module': None
+    }
+]
